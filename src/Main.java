0abb1eee1f9ecdb1238925ce378d8f195a51474a
Kostya Ivanov <konstantinivanow@yandex.ru>
2022-03-15 22:58:38 +0300

    Comments added

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Connection conn = getConnection();
 
-
+        //Test comments
         System.out.println("use DriverManager");
         if (conn != null) {
             Statement myStatement = createStatement(conn);
